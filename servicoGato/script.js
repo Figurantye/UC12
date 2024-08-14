74b875e037403caa0bf25e8f41a775b20ac2fae8
Gustavo Wazlawick <eva01.gustavo@gmail.com>
2024-08-14 11:30:42 -0300

    consumindo api de gatos

---
+++ servicoGato/script.js
@@ -0,0 +1,17 @@
+function showCatFact(photoId) {
+    //Variável que contém o link com o limite de 140 caracteres
+    const catFactsApiUrl = "https://catfact.ninja/fact?max_length=140"
+
+    fetch(catFactsApiUrl)
+        .then(response => response.json())
+        .then(data => {
+            //Extraindo o fato de gato da resposta.
+            const catFact = data.fact 
+
+            document.getElementById('catFactMessage').innerText = catFact
+        })
+        .catch(error => {
+            console.error(`Erro ao obter a frase: ${error}`);
+            document.getElementById('catFactMessage').innerText = 'Erro ao obter a frase'
+        })
+}
