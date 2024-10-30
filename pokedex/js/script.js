e56356249996bd7cf3e55574f0232d19f1ec241c
Gustavo Wazlawick <eva01.gustavo@gmail.com>
2024-10-30 11:55:54 -0300

    inciando a pokedex

---
+++ pokedex/js/script.js
@@ -0,0 +1,29 @@
+const imgPokemon = document.querySelector('#imgPokemon')
+const nomePokemon = document.querySelector('#nomePokemon')
+const numeroPokemon = document.querySelector('#numeroPokemon')
+const inputText = document.querySelector('#inputText')
+const form = document.querySelector('#form-busca')
+const buttonA = document.querySelector('#btnAnterior')
+const buttonP = document.querySelector('#btnProximo')
+const type = document.querySelector('#typePokemon')
+
+let IDPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+    const APIresponse = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    APIresponse => APIresponse.json()
+    const data = await APIresponse;
+    return data
+}
+
+const showPokemon = async (pokemon) => {
+    const dataPokemon = await fetchPokemon(pokemon);
+    IDPokemon = dataPokemon.id;
+    imgPokemon.src = dataPokemon.sprites.front_default;
+    const texto = document.createElement('span')
+    texto.textContent = dataPokemon.types[0].type.name
+    type.appendChild(texto)
+    nomePokemon.innerHTML = dataPokemon.name;
+}
+
+showPokemon('charizard')
