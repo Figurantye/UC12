c878fbdec5c2462b2f8f2827a4b2e572a8d11859
Gustavo Wazlawick <eva01.gustavo@gmail.com>
2024-10-31 11:55:30 -0300

    arrumando script

--- pokedex/js/script.js
+++ pokedex/js/script.js
@@ -2,7 +2,7 @@ const imgPokemon = document.querySelector('#imgPokemon')
 const nomePokemon = document.querySelector('#nomePokemon')
 const numeroPokemon = document.querySelector('#numeroPokemon')
 const inputText = document.querySelector("#inputText")
-const form = document.querySelector('#form-busca')
+const form = document.querySelector('.form-busca')
 const buttonA = document.querySelector('#btnAnterior')
 const buttonP = document.querySelector('#btnProximo')
 const type = document.querySelector('#typePokemon')
@@ -11,7 +11,6 @@ let IDPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
     const APIresponse = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    APIresponse => APIresponse.json()
     const data = await APIresponse.json();
     return data
 }
@@ -22,8 +21,22 @@ const showPokemon = async (pokemon) => {
     imgPokemon.src = dataPokemon.sprites.front_default;
     const texto = document.createElement('span')
     texto.textContent = dataPokemon.types[0].type.name
+    texto.className = 'adicionado'
     type.appendChild(texto)
+    const texto2 = document.createElement('span')
+    texto2.textContent = ` e ${dataPokemon.types[1].type.name}`
+    texto2.className = 'adicionado'
+    type.appendChild(texto2)
     nomePokemon.innerHTML = dataPokemon.name;
+    numeroPokemon.innerHTML = dataPokemon.id
 }
 
-showPokemon(inputText.value)
+inputText.addEventListener('input', () => 
+    IDPokemon = toString(inputText.value)
+)
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    type
+    showPokemon(inputText.value.toLowerCase());
+})
